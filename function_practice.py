d8739c413df2a241062d587d5403247dafe9bfd4
Afolabi01 <feerrup01@gmail.com>
2023-11-25 09:58:32 +0100

    resumption

---
+++ function_practice.py
@@ -0,0 +1,16 @@
+def impose(a,b,c):
+    x = (a**b) / c
+    return x
+
+def dare(a):
+    z = a + 4 + 7
+    return z
+
+def Print(A):
+    for a in A:
+        print(a+'1')
+        
+Print(['a','b','c'])
+
+print(dare(4))
+print(impose(24,3,2))
---
+++ last_fm_api.py
@@ -0,0 +1,16 @@
+#This is a simple illustration of using the request module to interact with an API(last fm)
+#First import the module
+import requests
+
+#create a request and save it in the response variable or any name you want to call it
+response = requests.get("http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=Harry Styles&api_key=&format=json")
+
+#Then derive your results in json form
+results = response.json()
+name = results['artist']['name']
+listeners = results['artist']['stats']['listeners']
+plays = results['artist']['stats']['playcount']
+
+#Print out your results using the print function
+print('This is ' + name)
+print('He has ' + listeners + ' listeners and his music has been played ' + plays + ' times')
